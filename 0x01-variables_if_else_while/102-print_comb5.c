76bc6cd92424ce57794517e136a9ad03433c83e8
UcheNwuchu <willnwuchu@gmail.com>
2023-10-06 20:06:03 -0700

    a program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  a program that prints all possible combinations of two two digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int hundreds;
+	int tens;
+	int ones;
+	int num;
+
+	for (num = 0; num < 1000; num++)
+	{
+		hundreds = num / 100;
+		tens = (num / 10) % 10;
+		ones = num % 10;
+
+		if (hundreds < tens && tens < ones)
+		{
+			putchar(hundreds + '0');
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (num < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
