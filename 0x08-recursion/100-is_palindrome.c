88b616a7552ae7d85f013588da9b0c8a607bd4b7
UcheNwuchu <willnwuchu@gmail.com>
2023-10-27 13:24:47 -0700

    a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int is_palindrome(char *s)
 {
 if (*s == 0)
 return (1);
-return (check_pali(s, 0, _strlen_recursion(s)));
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
 * _strlen_recursion - returns the length of a string
@@ -26,17 +26,17 @@ return (0);
 return (1 + _strlen_recursion(s + 1));
 }
 /**
-* check_pali - checks if characters recursively for palindrome
+* check_pal - checks if characters recursively for palindrome
 * @s: string to check
 * @i: iterator
 * @len: length of the string
 * Return: 1 if palindrome, 0 if not
 */
-int check_pali(char *s, int i, int len)
+int check_pal(char *s, int i, int len)
 {
 if (*(s + i) != *(s + len - 1))
 return (0);
 if (i >= len)
 return (1);
-return (check_pali(s, i + 1, len - 1));
+return (check_pal(s, i + 1, len - 1));
 }
